cd8ca322fb08c82f94dc059bd4fc2952d58663c2
Krishna1245679 <127098742+TanviBhasin@users.noreply.github.com>
2024-05-18 20:39:15 +0530

    n

--- frontend/src/components/auth/ApiServices.js
+++ frontend/src/components/auth/ApiServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL="http://localhost:4001/"
+export const BASE_URL="https://mern-stack-project-omega.vercel.app/"
 class ApiServices{
     getToken(){
         let obj={
